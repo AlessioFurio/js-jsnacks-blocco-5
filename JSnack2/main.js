8bee062ea976a7a46ca2a6c809417bc131cc2adf
Furio Alessio <alefurio@hotmail.it>
2020-11-02 15:36:45 +0100

    snack 2

---
+++ JSnack2/main.js
@@ -0,0 +1,19 @@
+
+
+//Creare un array di nomi
+var arrayNomi = ['Alessio', 'Flavio', 'Simone', 'Cecilia'];
+
+//e chiedere all'utente due numeri.
+var numero1 = parseInt(prompt('inserisci primo numero'));
+var numero2 = parseInt(prompt('inserisci secondo numero'));
+
+
+//Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall'utente.
+var arrayNuovo = [];
+for (var i = numero1; i < arrayNomi.length; i++) {
+    if (i <= numero2) {
+        arrayNuovo.push(arrayNomi[i]);
+    }
+}
+
+console.log(arrayNuovo);
